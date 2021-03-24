f536d70fdbc50e70ee34fa0be8d3394c8e7dcaf8
okstring <milkoi7801@gmail.com>
2021-03-24 16:16:24 +0900

    Make: Doodle Codable 객체 구현, DoodleViewController 랜덤 색상 테스트 확인, encode json(Doodle)

--- PhotosApp/PhotosApp/DoodleViewController.swift
+++ PhotosApp/PhotosApp/DoodleViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import Photos
 
 class DoodleViewController: UICollectionViewController {
+    var doodles = [Doodle]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +20,45 @@ class DoodleViewController: UICollectionViewController {
                  action: #selector(closeButtonPushed))
         self.collectionView.backgroundColor = .darkGray
         
+        loadDoodles()
+    }
+    
+    func loadDoodles() {
+        guard let photosData = NSDataAsset(name: "doodle") else { return }
+        do {
+            self.doodles = try JSONDecoder().decode([Doodle].self, from: photosData.data)
+        } catch {
+            self.doodles = [Doodle]()
+            return
+        }
     }
     
     @objc func closeButtonPushed() {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension DoodleViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return doodles.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as? CustomCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        cell.backgroundColor = getRandomColor()
+        
+        return cell
+    }
+    
+    func getRandomColor() -> UIColor{
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
 }
 
