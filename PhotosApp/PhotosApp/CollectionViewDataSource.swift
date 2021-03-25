2061e35a9cee4d264a281bf87881f1c625f97bfa
dahun-lee-daji <ssss6596@gmail.com>
2021-03-25 14:24:07 +0900

    불필요한 코드 제거
    backgroundColor를 랜덤하게 할당하는 메소드 제거

--- PhotosApp/PhotosApp/CollectionViewDataSource.swift
+++ PhotosApp/PhotosApp/CollectionViewDataSource.swift
@@ -31,15 +31,7 @@ extension CollectionViewDataSource: UICollectionViewDataSource {
                                   resultHandler: { image, _ in
                                     cell.imageView.image = image
                                   })
-        cell.backgroundColor = getRandomColor()
         return cell
-    }
-    
-    func getRandomColor() -> UIColor{
-        let randomRed:CGFloat = CGFloat(drand48())
-        let randomGreen:CGFloat = CGFloat(drand48())
-        let randomBlue:CGFloat = CGFloat(drand48())
-        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
 }
 
